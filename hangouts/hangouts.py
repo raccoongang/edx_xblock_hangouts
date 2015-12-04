bbc5150fa60744cc5fd82191ac75dd11928cf3b3
Oksana Slusarenro <oksana.slu@gmail.com>
2015-12-04 11:53:20 +0200

    Data transfer in js.

--- hangouts/hangouts.py
+++ hangouts/hangouts.py
@@ -1,73 +1,73 @@
-"""TO-DO: Write a description of what this XBlock is."""
-
 import pkg_resources
 
+from django.template import Template, Context
+from student.models import CourseEnrollment
 from xblock.core import XBlock
-from xblock.fields import Scope, String, Integer
+from xblock.fields import Scope, String
 from xblock.fragment import Fragment
+from xblockutils.studio_editable import StudioEditableXBlockMixin
 
 
-class HangoutsXBlock(XBlock):
-    """
-    TO-DO: document what your XBlock does.
-    """
+# Make '_' a no-op so we can scrape strings
+_ = lambda text: text
 
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
+
+class HangoutsXBlock(StudioEditableXBlockMixin, XBlock):
 
     display_name = String(
-        help="The name students see. This name appears in the course ribbon and as a header for the presentation.",
+        display_name=_("Display Name"),
+        help=_("The name students see. This name appears in the course ribbon and as a header for the presentation."),
         default="Hangouts On Air",
-        scope=Scope.settings
+        scope=Scope.settings,
     )
 
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
-    )
+    editable_fields = ('display_name',)
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
-    # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
-        """
-        The primary view of the HangoutsXBlock, shown to students
-        when viewing courses.
-        """
-        html = self.resource_string("static/html/hangouts.html")
-        frag = Fragment(html.format(self=self))
+        if context is None:
+            context = {}
+
+        is_course_staff = False
+        if self.runtime.get_user_role() in ['instructor', 'staff']:
+            is_course_staff = True
+
+        context.update({
+            'self': self,
+            'is_course_staff': is_course_staff
+        })
+
+        template = self.render_template("static/html/hangouts.html", context)
+        frag = Fragment(template)
         frag.add_css(self.resource_string("static/css/hangouts.css"))
         frag.add_javascript(self.resource_string("static/js/src/hangouts.js"))
-        frag.initialize_js('HangoutsXBlock')
+
+        emails_enrolled = list(CourseEnrollment.objects.users_enrolled_in(course_id=self.course_id)\
+            .values_list('email', flat=True))
+
+        json_args = {
+            'is_course_staff': is_course_staff,
+            'title': self.display_name,
+            'emails_enrolled': emails_enrolled,
+            'start_date': self.start.strftime('%Y-%m-%dT%H:%M:%S.%f') if self.start else None
+        }
+        frag.initialize_js('HangoutsXBlock', json_args=json_args)
         return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
-        """
-        An example handler, which increments the data.
-        """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
+    def render_template(self, template_path, context):
+        template_str = self.resource_string(template_path)
+        template = Template(template_str)
+        return template.render(Context(context))
 
-        self.count += 1
-        return {"count": self.count}
-
-    # TO-DO: change this to create the scenarios you'd like to see in the
-    # workbench while developing your XBlock.
     @staticmethod
     def workbench_scenarios():
-        """A canned scenario for display in the workbench."""
         return [
             ("HangoutsXBlock",
              """<vertical_demo>
-                <hangouts/>
-                <hangouts/>
                 <hangouts/>
                 </vertical_demo>
              """),
